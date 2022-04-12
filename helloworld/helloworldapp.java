31c9ed7ba1f1fb6cd6b9ff417d5daf83d48625f8
6kaki <juniyook@korea.ac.kr>
2022-04-12 14:27:49 +0900

    java_hello_world

---
+++ helloworld/helloworldapp.java
@@ -0,0 +1,7 @@
+public class helloworldapp{ //파일 실행 파일 이름과 같은 class로 만들어주어야한다.
+	public static void main(String[] args) { //중괄호 안에 있는 코드를 실행.
+		System.out.println("Hello World!!");
+	}
+}
+//저장을 누르면 navigator에 .class가 생긴다.
+//run -> 글자가 출력되는 것을 볼 수 있다.
