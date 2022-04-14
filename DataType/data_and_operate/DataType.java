52e2b8a17fd950f8023283ef75516cc28b69fb9b
6kaki <juniyook@korea.ac.kr>
2022-04-14 21:18:57 +0900

    data and operator

---
+++ DataType/data_and_operate/DataType.java
@@ -0,0 +1,18 @@
+public class DataType{
+	public static void main(String[] args) {
+		System.out.println(6); //숫자를 표현
+		System.out.println("six"); //문자열 String
+		
+		System.out.println("6"); //String 6
+		System.out.println(6+6);
+		System.out.println("6" + "6");
+		
+		System.out.println(6*6);
+		//System.out.println("6" * "6");
+		
+		System.out.println("1111".length());
+		//System.out.println(1111.length());
+		
+		
+		}
+}
---
+++ DataType/data_and_operate/data_and_operate/Number.java
@@ -0,0 +1,18 @@
+package data_and_operate;
+
+public class Number {
+
+	public static void main(String[] args) {
+		//Operator
+		System.out.println(6 - 2); //4
+		System.out.println(6 + 2); //8
+		
+		System.out.println(6 * 2); // 12
+		System.out.println(6 / 2); // 3
+		
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(Math.PI)); //3.14에서 소수점을 자르기.
+		System.out.println(Math.ceil(Math.PI)); //올림.
+	}
+
+}
